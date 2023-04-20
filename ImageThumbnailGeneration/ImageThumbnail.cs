40619f3356b9517b852cac3cdd43d13e800193aa
Rahul Bagal <rahul@incubxperts.com>
2023-04-20 12:10:48 +0530

    Initial commit of thumbnail generation code

---
+++ ImageThumbnailGeneration/ImageThumbnail.cs
@@ -0,0 +1,44 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
+using System.IO;
+
+namespace ImageThumbnailGeneration
+{
+    public class ImageThumbnailGenerator
+    {
+        /// <summary>
+        /// Generates a thumbnail from an image. 
+        /// Accepts image as JPG / PNG formats and returns memory stream of thumbnail
+        /// It maintains aspect ratio of the image while generating thumbnail
+        /// </summary>
+        /// <param name="imageStream">Image file as stream</param>
+        /// <param name="width">Width in pixels for thumbnail</param>
+        /// <param name="height">Height in pixels for thumbnail</param>
+        /// <returns></returns>
+        public static Stream GenerateThumbnail(Stream imageStream, int width, int height)
+        {
+            // Load the source image from the input stream
+            var image = Image.Load(imageStream);
+
+            // Resize the image to create a thumbnail
+            image.Mutate(x => x.Resize(new ResizeOptions
+            {
+                Size = new Size(width, height),
+                Mode = ResizeMode.Max
+            }));
+
+            // Save the thumbnail image to a memory stream
+            var thumbnailStream = new MemoryStream();
+            image.Save(thumbnailStream, new JpegEncoder());
+
+            // Reset the memory stream position to the beginning
+            thumbnailStream.Position = 0;
+
+            // Return the thumbnail memory stream
+            return thumbnailStream;
+        }
+    }
+
+}
+
---
+++ ImageThumbnailGeneration/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace ImageThumbnailGeneration
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("This is a thumbnail generation sample code, It generates thumbnail from image");
+            // Specify source file of original image and target file where thumbnail image is generated.
+            string InputImageFile = Environment.CurrentDirectory + "\\temp\\large-image.jpg";
+            Console.WriteLine("Generating thumbnail for image file " + InputImageFile);
+            string OutputThumbnailImageFile = Environment.CurrentDirectory + "\\temp\\tumb-image.jpg";
+            GenerateThumbnailFromFile(InputImageFile, OutputThumbnailImageFile);
+            Console.WriteLine("Thumbnail created at :");
+            Console.WriteLine(OutputThumbnailImageFile);
+        }
+
+        /// <summary>
+        /// Generates thumbnail from image file provided
+        /// </summary>
+        /// <param name="InputImageFile">Input image file</param>
+        /// <param name="OutputThumbnailImageFile">Output file location for thumbnail image. File will be created/overwritten</param>
+        static void GenerateThumbnailFromFile(string InputImageFile, string OutputThumbnailImageFile)
+        {
+            // Open source file as file stream.
+            using FileStream inputFileStream = new(InputImageFile, FileMode.Open);
+            // Call image thumbnail generator to generate thumnail 
+            using (var thumbnailMemStream = ImageThumbnailGenerator.GenerateThumbnail(inputFileStream, 200, 200))
+            {
+                var outputFileStream = new FileStream(OutputThumbnailImageFile, FileMode.Create, FileAccess.Write);
+                thumbnailMemStream.CopyTo(outputFileStream);
+                outputFileStream.Dispose();
+                thumbnailMemStream.Dispose();
+            }
+            // Close open streams. 
+            inputFileStream.Dispose();
+        }
+    }
+}
